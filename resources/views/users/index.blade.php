7d73e37422c13cf3eac96cc4194e9936bee73d27
PovilasKorop <povilask007@gmail.com>
2021-09-16 08:40:32 +0300

    Small cleanup

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,9 +19,7 @@
                             @foreach ($users as $user)
                                 <tr>
                                     <td>{{ $user->name }}</td>
-                                    <td>
-                                        {{ $user->email }}
-                                    </td>
+                                    <td>{{ $user->email }}</td>
                                 </tr>
                             @endforeach
                             </tbody>
