19028ffcce3f37e461ffe34349eafecb45fcee36
Nerijus <krekas@gmail.com>
2021-09-16 09:10:37 +0300

    Remove old password field

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -47,20 +47,6 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="old_password" class="col-md-4 col-form-label text-md-right">{{ __('Old password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="old_password" type="password" class="form-control @error('old_password') is-invalid @enderror" name="old_password">
-
-                                @error('old_password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New password') }}</label>
 
                             <div class="col-md-6">
