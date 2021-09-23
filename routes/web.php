8a6273fbc4cda16ce7c239206d210394158e28a1
Abdul Rafay Modi <74718326+AlphaRomeoMike@users.noreply.github.com>
2021-09-23 12:49:02 +0500

    Simplified the FQN in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +22,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+Route::get('users', [UserController::class, 'index'])->name('users.index');
 
-Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
-Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
+Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
